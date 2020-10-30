f8ed9505bd6d7cc05946c420fb09bd99ab7477d2
Martin <martin.vsa@gmail.com>
2020-10-30 00:42:21 -0300

    Json error handler

--- app/services/SessionService.scala
+++ app/services/SessionService.scala
@@ -1,20 +1,24 @@
 package services
 
 import javax.inject.{Inject, Singleton}
+import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
+
 import scala.concurrent.Future
 
 case class AuthResponse(auth: Boolean, token: String)
 
 @Singleton
-class SessionService @Inject()(ws: WSClient) {
+class SessionService @Inject()(ws: WSClient, configuration: Configuration) {
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
   def withSession[A](f: AuthResponse => Future[A]): Future[A] = {
     implicit val personReads = Json.reads[AuthResponse]
+    val apiKey = configuration.get[String]("apikey")
+
     val data = Json.obj(
-      "apiKey" -> "23567b218376f79d9415",
+      "apiKey" -> apiKey,
     )
     val request = ws.url("http://interview.agileengine.com/auth").post(data)
     request.flatMap {
